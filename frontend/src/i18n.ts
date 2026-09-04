0b7b71c5ec782e7f94a1fffa1ff056b3bed144ff
h3rhex <149667352+H3rHex@users.noreply.github.com>
2026-09-04 19:36:49 +0200

    chore: configure i18n & tailwind

---
+++ frontend/src/i18n.ts
@@ -0,0 +1,24 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+
+import es from './langs/es.json';
+
+i18n
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        resources: {
+            es: { translation: es },
+        },
+        fallbackLng: 'es',
+        interpolation: {
+            escapeValue: false,
+        },
+        detection: {
+            order: ['localStorage', 'navigator', 'htmlTag', 'path', 'subdomain'],
+            caches: ['localStorage'],
+        }
+    });
+
+export default i18n;
